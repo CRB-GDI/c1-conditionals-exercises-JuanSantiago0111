a8af6625de3abb7702c73dd1a359a242ab390498
JuanSantiago0111 <145376152+JuanSantiago0111@users.noreply.github.com>
2024-03-19 11:44:28 -0400

    done

--- Exercise7/Program.cs
+++ Exercise7/Program.cs
@@ -21,12 +21,28 @@ namespace Exercise7
                 // Do Not change code above this line.  Add your code below
 
                 // Add code to prompt for and get user's password.
+                Console.Write("Enter the password of the dooor!");
 
+                string response = Console.ReadLine();
+                
+                if (response == correctPassword)
+                {
+                    Console.WriteLine("you have been granted entry");
+                    doorLocked = false;
+                }
 
+                else if (response !=  correctPassword && response != "")
+                {
+                    Console.WriteLine("nope!");
+                }
+                else
+                {
+                    Console.WriteLine("You must enter something");
+                }
 
 
                 //Check if the password is correct or not
-
+               
 
 
 
